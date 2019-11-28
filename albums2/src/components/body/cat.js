3c6dca75182717d4eeb52aae01eb830ecb12aba0
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-11-28 16:08:38 +0700

    changed class component into functional component because cat is static

--- albums2/src/components/body/cat.js
+++ albums2/src/components/body/cat.js
@@ -1,16 +1,13 @@
 import React, {Component} from 'react'
 import {View,Text,StyleSheet} from 'react-native'
 
-class Cat extends Component {
-  render() {
-    console.log(this.props.cat)
+const Cat = props => {
     return(
       <View>
-      <Text>Name: {this.props.cat.name}</Text>
-      <Text>Affection Level: {this.props.cat.affection_level}/5</Text>
+      <Text>Name: {props.cat.name}</Text>
+      <Text>Affection Level: {props.cat.affection_level}/5</Text>
       </View>
     )
   }
-}
 
 export default Cat;
